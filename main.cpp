f8321519d8e1da9be167acd5f31ef65dd880caea
maloleg <33466049+maloleg@users.noreply.github.com>
2020-04-05 16:49:08 +0300

    Понятия не имею, как это всё использовать, но оно компилиться, а так разделил объявление и тела методов классов, вроде убрал комменты, теперь всё вроде выглядить вполне себе красиво

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-#define SIZE 1024
 /* порты [0,1023] - системные, [1024,65535] доступны пользователю */
 #define PORT  3490
 /* размер очереди [listen()] входящих соединений ожидающих приема */
@@ -17,64 +16,27 @@
 class Socket{
 private:
     int sockfd;
-
 protected:
     bool good;
-    int sock = sockfd;
-
 public:
+    int DGetter();
 
-    int DGetter(){return sockfd;}
+    void DSetter(int s);
 
-    void DSetter(int s){sockfd = s;}
+    Socket& Read(char* Text, std::streamsize TextSize);
 
-    Socket& Read(char* Text, std::streamsize TextSize){
-        do
-        {
-            char data[1];
-            int ret;
-            Text = "";
-//
-            memset(data, 0x00, 1);
+    Socket& Write(const char* a, std::streamsize b);
 
-            /* чтение данных */
-            for (int i = 0; i < TextSize; i++) {
-                if ((ret = recv(sockfd, data, 1, 0)) == -1) {
-                    perror("recv()");
-                    break;
-                }
-                else{
-                    Text += data[0];
-                }
-            }
-        }
-        while(0);
-        return dynamic_cast<Socket&> (*this);
-    }
-    Socket& Write(const char* a, std::streamsize b){
-            /* отправка данных по установленному каналу связи */
-            for (int i = 0; i < b; i++) {
-                if (send(sockfd, reinterpret_cast<const void *>(a[i]), 1, 0) == -1) {
-                    perror("send()");
-                }
-            }
-            return dynamic_cast<Socket&> (*this);
-    }
     friend Socket& operator >> (Socket&, std::string&);
 
     friend Socket& operator << (Socket&, std::string&);
 
-    void Shutdown(){
-        if (shutdown(sockfd, SHUT_RDWR) == -1){
-            perror("shutdown()");
-        }
-    }
+    void Shutdown();
 };
 
 Socket& operator >> (Socket& sock, std::string& Text){
     char *cstr = new char[Text.length() + 1];
     strcpy(cstr, Text.c_str());
-// do stuff
 
     sock.Read(cstr, Text.length() + 1);
     delete [] cstr;
@@ -87,77 +49,127 @@ Socket& operator << (Socket& sock, std::string& TextStream){
     return sock;
 }
 
+Socket &Socket::Read(char *Text, std::streamsize TextSize) {
+    do
+    {
+        char data[1];
+        int ret;
+        Text = "";
+
+        memset(data, 0x00, 1);
+
+        for (int i = 0; i < TextSize; i++) {
+            if ((ret = recv(sockfd, data, 1, 0)) == -1) {
+                perror("recv()");
+                break;
+            }
+            else{
+                Text += data[0];
+            }
+        }
+    }
+    while(0);
+    return dynamic_cast<Socket&> (*this);
+}
+
+Socket &Socket::Write(const char *a, std::streamsize b) {
+    for (int i = 0; i < b; i++) {
+        if (send(sockfd, reinterpret_cast<const void *>(a[i]), 1, 0) == -1) {
+            perror("send()");
+        }
+    }
+    return dynamic_cast<Socket&> (*this);
+}
+
+void Socket::Shutdown() {
+    if (shutdown(sockfd, SHUT_RDWR) == -1){
+        perror("shutdown()");
+    }
+}
+
+void Socket::DSetter(int s) {sockfd = s;}
+
+int Socket::DGetter() {return sockfd;}
+
 class ClientSocket : Socket{
 private:
     struct sockaddr_in peer;
 public:
 
-    ClientSocket(int SockD, int port, char* ip){
-        DSetter(SockD);
+    ClientSocket(int SockD, int port, char* ip);
 
-        memset(&peer, 0x00, sizeof(struct sockaddr_in));
-        peer.sin_family      = AF_INET;
-        peer.sin_port        = htons(port);
-        peer.sin_addr.s_addr = inet_addr(ip);
-    }
+    ClientSocket();
 
-    ClientSocket(){
-        memset(&peer, 0x00, sizeof(struct sockaddr_in));
-        peer.sin_family      = AF_INET;
-        peer.sin_port        = htons(PORT);
-        peer.sin_addr.s_addr = inet_addr("127.0.0.1");
-    }
-
-    void Connect(){
-        if (connect(DGetter(), (struct sockaddr*)&peer, sizeof(struct sockaddr_in)) == -1){
-            perror("connect()");
-            exit(1);
-        }
-    }
+    void Connect();
 };
+
+ClientSocket::ClientSocket(int SockD, int port, char *ip) {
+    DSetter(SockD);
+
+    memset(&peer, 0x00, sizeof(struct sockaddr_in));
+    peer.sin_family      = AF_INET;
+    peer.sin_port        = htons(port);
+    peer.sin_addr.s_addr = inet_addr(ip);
+}
+
+ClientSocket::ClientSocket() {
+    memset(&peer, 0x00, sizeof(struct sockaddr_in));
+    peer.sin_family      = AF_INET;
+    peer.sin_port        = htons(PORT);
+    peer.sin_addr.s_addr = inet_addr("127.0.0.1");
+}
+
+void ClientSocket::Connect() {
+    if (connect(DGetter(), (struct sockaddr*)&peer, sizeof(struct sockaddr_in)) == -1){
+        perror("connect()");
+        exit(1);
+    }
+}
 
 class ServerSocket : public Socket{
 private:
     struct sockaddr_in self;
 public:
-    void Bind(){
-        if(bind(DGetter(), (struct sockaddr*)&self, sizeof(struct sockaddr_in)) == -1){
-            perror("bind()");
-            exit(1);
-        }
-    }
-    void Listen(){
-        if(listen(DGetter(), QUEUE) == -1){
-            perror("listen()");
-            exit(1);
-        }
-    }
-    ClientSocket Accept(){
-        socklen_t addrlen = sizeof(struct sockaddr_in);
-        int cli_sock;
-
-        if((cli_sock = accept(DGetter(), (struct sockaddr*)&self, &addrlen)) == -1)
-        {
-            perror("accept()");
-            exit(1);
-        }
-
-        return ClientSocket(cli_sock, PORT, "127.0.0.1");
-
-    }
+    void Bind();
+    void Listen();
+    ClientSocket Accept();
 };
 
+void ServerSocket::Bind() {
+    if(bind(DGetter(), (struct sockaddr*)&self, sizeof(struct sockaddr_in)) == -1){
+        perror("bind()");
+        exit(1);
+    }
+}
+
+void ServerSocket::Listen() {
+    if(listen(DGetter(), QUEUE) == -1){
+        perror("listen()");
+        exit(1);
+    }
+}
+
+ClientSocket ServerSocket::Accept() {
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    int cli_sock;
+
+    if((cli_sock = accept(DGetter(), (struct sockaddr*)&self, &addrlen)) == -1)
+    {
+        perror("accept()");
+        exit(1);
+    }
+
+    return ClientSocket(cli_sock, PORT, "127.0.0.1");
+
+}
+
 int main() {
-    //std::cout << "Hello, World!" << std::endl;
     ClientSocket a;
     ServerSocket b;
 
-    a.Connect();
+    //a.Connect();
 
-    while (1) {
-
-        a.Write("Hello world!", 12);
-    }
+    std::cout << "I really didn't get, why we should return reference to a class with methods Write and Read.\nAnd if we do so, how we should use it?";
 
     return 0;
 }
